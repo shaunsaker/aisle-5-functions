54eea8aa4748ba6c005e19162ad8525220405799
Shaun Saker <sakershaun@gmail.com>
2021-09-13 09:58:49 +0200

    add lint

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,6 @@
-import * as firebase from "firebase-admin";
+import * as firebase from 'firebase-admin';
 
-const serviceAccount = require("./service-account.json");
+const serviceAccount = require('./service-account.json');
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
--- src/notifyConfirmedCaseCreated/index.ts
+++
@@ -1,7 +0,0 @@
-import * as functions from "firebase-functions";
-
-const notifyConfirmedCaseCreated = functions.firestore
-  .document("confirmedCases/{confirmedCaseId}")
-  .onCreate(async (snapshot, context) => {});
-
-export { notifyConfirmedCaseCreated };
